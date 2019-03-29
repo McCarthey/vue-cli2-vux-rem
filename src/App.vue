2b67c60514b1a64dc9568480dc918b6ac1aa62a9
Randall <randall@duomai.com>
2019-03-29 17:11:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { calRem } from "./rem";
+
+const fontSize = 32
+
+export default {
+  name: "app",
+  beforeCreate() {
+    calRem(fontSize)
+    window.onresize = () => calRem(fontSize)
+  }
+};
+</script>
+
+<style lang="less">
+@import "~vux/src/styles/reset.less";
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
---
+++ src/rem.js
@@ -0,0 +1,5 @@
+
+export function calRem(baseSize) {
+    const scale = document.documentElement.clientWidth / 750
+    document.documentElement.style.fontSize = (baseSize * Math.min(scale, 2)) + 'px'
+}
